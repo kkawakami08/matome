a68ba4c2e73077c43e8f46d07833fc26508a2f2d
Kaori Kawakami <k.kawakami08@gmail.com>
2025-04-02 07:19:23 -0700

    tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <Button>HomePage</Button>;
+  return (
+    <div>
+      <h1>Matome</h1>
+    </div>
+  );
 };
 
 export default HomePage;
